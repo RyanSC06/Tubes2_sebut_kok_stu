ad0024321b59c85f7f89b335dba944932fda8f8c
SaddamAnnais <saddamannaiss@gmail.com>
2023-03-20 10:43:17 +0700

    Input section

---
+++ src/FrontEnd/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FrontEnd
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.ResizeMode = ResizeMode.NoResize;
+        }
+
+        private void btnOpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            // Create OpenFileDialog 
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+            // Set filter for file extension and default file extension 
+            dlg.DefaultExt = ".txt";
+            dlg.Filter = "Text files (*.txt) | *.txt";
+
+
+            // Display OpenFileDialog by calling ShowDialog method 
+            Nullable<bool> result = dlg.ShowDialog();
+
+
+            // Get the selected file name and display in a TextBox 
+            if (result == true)
+            {
+                // Open document 
+                string path= dlg.FileName;
+                btnOpenFile.Content = System.IO.Path.GetFileName(path);
+            }
+        }
+
+        private void rbBfs_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
