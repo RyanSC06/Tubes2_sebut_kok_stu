698c56af953d0ebc98c46afdc63cbd010ef0af78
yansans <66671259+yansans@users.noreply.github.com>
2023-03-20 23:09:42 +0700

    update attr, method

--- src/Graph.cs
+++ src/Graph.cs
@@ -1,24 +1,24 @@
 public class Graph{
 
-    public List<Point> nodes {get; set;}
+    public List<Point> nodes;
 
-    public Dictionary<Point,List<Point>> adjList {get; set;}
+    public List<Point> Nodes {
+        get { return nodes; }
+        set { nodes = value; }
+    }
 
-    // public Dictionary<Point, Boolean> visited {get; set;}
+    public Dictionary<Point,List<Point>> adjList;
 
-    // public static int node_visited = 0;
-
-    private List<Point> path = new List<Point>(); 
+    public Dictionary<Point, List<Point>> AdjList {
+        get { return adjList; }
+        set { adjList = value; }
+    }
 
     public Graph(List<Point> nodes){
         this.nodes = nodes;
         adjList = new Dictionary<Point, List<Point>>();
         foreach (Point node in nodes){
             adjList.Add(node, new List<Point>());
-        }
-        visited = new Dictionary<Point, Boolean>();
-        foreach (Point node in nodes){
-            visited.Add(node, false);
         }
     }
 
@@ -34,49 +34,13 @@ public class Graph{
         return adjList[node];
     }
 
-    // public void setVisited(Point node){
-    //     visited[node] = true;
-    //     node_visited++;
-    // }
-
-    // public bool isVisited(Point node){
-    //     return visited[node];
-    // }
-
-    // public void resetVisited(){
-    //     foreach (Point node in nodes){
-    //         visited[node] = false;
-    //     }
-    //     node_visited = 0;
-    // }
-
-    public void addPath(Point node){
-        path.Add(node);
-    }
-
-    public void resetPath(){
-        path.Clear();
-    }
-
-    public List<Point> getPath(){
-        return path;
-    }
-    public void printPath(){
-        foreach (Point node in path){
-            Console.Write(node.X + "," + node.Y);
-            if (node != path[path.Count-1])
-            Console.Write(" -> ");
-        }
-        Console.WriteLine();
-    }
-
     public static void Main (string[] args){
         // point test
         Point p = new Point(1, 2, TypeGrid.KrustyKrab);
         Console.WriteLine(p.X);
         Console.WriteLine(p.Y);
         Console.WriteLine(p.Type);
-        Console.WriteLine(p.found);
+        Console.WriteLine(p.Found);
 
         // set 
         p.X = 3;
@@ -86,7 +50,7 @@ public class Graph{
         Console.WriteLine(p.X);
         Console.WriteLine(p.Y);
         Console.WriteLine(p.Type);
-        Console.WriteLine(p.found);
+        Console.WriteLine(p.Found);
 
         // enum test
         Console.WriteLine(TypeGrid.KrustyKrab);
@@ -120,40 +84,6 @@ public class Graph{
         Console.WriteLine(g.getNeighbours(p3).Count);
         Console.WriteLine(g.getNeighbours(p4).Count);
 
-        Console.WriteLine(g.isVisited(p1));
-        Console.WriteLine(g.isVisited(p2));
-        Console.WriteLine(g.isVisited(p3));
-        Console.WriteLine(g.isVisited(p4));
-
-        g.setVisited(p1);
-        g.setVisited(p2);
-        g.setVisited(p3);
-        g.setVisited(p4);
-
-        Console.WriteLine(g.isVisited(p1));
-        Console.WriteLine(g.isVisited(p2));
-        Console.WriteLine(g.isVisited(p3));
-        Console.WriteLine(g.isVisited(p4));
-
-        g.resetVisited();
-
-        Console.WriteLine(g.isVisited(p1));
-        Console.WriteLine(g.isVisited(p2));
-        Console.WriteLine(g.isVisited(p3));
-        Console.WriteLine(g.isVisited(p4));
-
-
-        // path test
-        g.addPath(p1);
-        g.addPath(p2);
-        g.addPath(p3);
-        g.addPath(p4);
-
-        g.printPath();
-
-        g.resetPath();
-
-        g.printPath();
     }
 
 }
